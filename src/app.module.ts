533aa7e69ff54c72167102cf24d7bf12b466b1f2
kareraolivier <kareraolivier@gmail.com>
2024-01-11 09:50:44 +0200

    update mongo link

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module, ValidationPipe } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { mongoUrl } from "./config";
+import { mongoUrl } from "./config/config";
 import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
 import { RadiologyModule } from "./radiology/radiology.module";
