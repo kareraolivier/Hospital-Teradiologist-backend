942cdf9d656224a0278bde7927747ce3d64043aa
kareraolivier <kareraolivier@gmail.com>
2024-06-02 09:21:36 +0200

    feat: add login attempts

--- src/login-attempts/schemas/login-attempt.entity.ts
+++ src/login-attempts/schemas/login-attempt.entity.ts
@@ -1,20 +1,19 @@
 import * as mongoose from "mongoose";
-export const loginAttemptSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+export const loginAttemptSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    ipAddress: {
+      type: String,
+    },
+    successful: {
+      type: Boolean,
+      required: true,
+    },
   },
-  ipAddress: {
-    type: String,
-  },
-  successful: {
-    type: Boolean,
-    required: true,
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("LoginAttempt", loginAttemptSchema);
